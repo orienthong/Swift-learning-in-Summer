49deeabcdef44c68f395108ea823642e5db03fc7
scau_ox <645670644@qq.com>
2016-07-18 23:51:34 +0800

    Basic search

--- CandySearch/CandySearch/MasterViewController.swift
+++ CandySearch/CandySearch/MasterViewController.swift
@@ -27,15 +27,33 @@ class MasterViewController: UITableViewController {
   // MARK: - Properties
   var detailViewController: DetailViewController? = nil
   var candies = [Candy]()
+  var filteredCandies = [Candy]()
+  let searchController = UISearchController(searchResultsController: nil)
   
   // MARK: - View Setup
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    candies = [
+        Candy(category:"Chocolate", name:"Chocolate Bar"),
+        Candy(category:"Chocolate", name:"Chocolate Chip"),
+        Candy(category:"Chocolate", name:"Dark Chocolate"),
+        Candy(category:"Hard", name:"Lollipop"),
+        Candy(category:"Hard", name:"Candy Cane"),
+        Candy(category:"Hard", name:"Jaw Breaker"),
+        Candy(category:"Other", name:"Caramel"),
+        Candy(category:"Other", name:"Sour Chew"),
+        Candy(category:"Other", name:"Gummi Bear")
+    ]
 
     if let splitViewController = splitViewController {
       let controllers = splitViewController.viewControllers
       detailViewController = (controllers[controllers.count - 1] as! UINavigationController).topViewController as? DetailViewController
     }
+    searchController.searchResultsUpdater = self
+    searchController.dimsBackgroundDuringPresentation = false
+    definesPresentationContext = true
+    tableView.tableHeaderView = searchController.searchBar
   }
   
   override func viewWillAppear(animated: Bool) {
@@ -53,13 +71,22 @@ class MasterViewController: UITableViewController {
   }
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if searchController.active && searchController.searchBar.text != "" {
+        return filteredCandies.count
+    }
     return candies.count
   }
   
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
     
-    let candy = candies[indexPath.row]
+    let candy : Candy
+    if searchController.active && searchController.searchBar.text != "" {
+        candy = filteredCandies[indexPath.row]
+    } else {
+        candy = candies[indexPath.row]
+    }
+    
     cell.textLabel!.text = candy.name
     cell.detailTextLabel!.text = candy.category
     return cell
@@ -69,7 +96,13 @@ class MasterViewController: UITableViewController {
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     if segue.identifier == "showDetail" {
       if let indexPath = tableView.indexPathForSelectedRow {
-        let candy = candies[indexPath.row]
+        let candy : Candy
+        if searchController.active && searchController.searchBar.text != "" {
+            candy = filteredCandies[indexPath.row]
+        }
+        else {
+            candy = candies[indexPath.row]
+        }
         let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
         controller.detailCandy = candy
         controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem()
@@ -77,6 +110,21 @@ class MasterViewController: UITableViewController {
       }
     }
   }
+    //MARK: - searchFunc
+    func filterContentForSearchText(searchText: String,score: String = "All") {
+        filteredCandies = candies.filter{
+            candy in
+            return candy.name.lowercaseString.containsString(searchText.lowercaseString)
+        }
+        tableView.reloadData()
+    }
   
 }
+//Use the UISearchResultsUpdating protocol to update search results based on information the user enters into the search bar.
+extension MasterViewController : UISearchResultsUpdating {
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+}
+
 
