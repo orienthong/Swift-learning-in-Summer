7f7cf8d7b73e4df685826cb56c4ea0d41f9aceaf
scau_ox <645670644@qq.com>
2016-07-24 22:58:27 +0800

    resize the minSize and maxSize standBy the device's width and heigh.

--- ScrollView - practice/ScrollView - practice/ViewController.swift
+++ ScrollView - practice/ScrollView - practice/ViewController.swift
@@ -11,38 +11,60 @@ import UIKit
 class ViewController: UIViewController {
     
     var originLabel = UILabel(frame: CGRect(x: 20, y: 30, width: 0, height: 0))
-
+    let imageView = UIImageView(image: UIImage(named: "zombies.jpg"))
+    var scrollView = UIScrollView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let imageView = UIImageView(image: UIImage(named: "zombies.jpg"))
-        let srollView = UIScrollView(frame: view.bounds)
-        srollView.autoresizingMask = [.FlexibleWidth , .FlexibleHeight]
-        srollView.backgroundColor = UIColor.blackColor()
-        srollView.contentSize = imageView.bounds.size
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.autoresizingMask = [.FlexibleWidth , .FlexibleHeight]
+        scrollView.backgroundColor = UIColor.blackColor()
+        scrollView.contentSize = imageView.bounds.size
         
-        srollView.addSubview(imageView)
-        view.addSubview(srollView)
+        scrollView.addSubview(imageView)
+        view.addSubview(scrollView)
         
         //set content offSet
-        srollView.contentOffset = CGPoint(x: 60, y: 342.5)
+        scrollView.contentOffset = CGPoint(x: 60, y: 342.5)
+        
+        
         
         
         originLabel.backgroundColor = UIColor.blackColor()
         originLabel.textColor = UIColor.whiteColor()
         
         view.addSubview(originLabel)
-        srollView.delegate = self
+        scrollView.delegate = self
+        
+        setZoomParametersForSize(scrollView.bounds.size)
         
     }
-
+    func setZoomParametersForSize(scrollViewSize: CGSize){
+        let imageViewsize = imageView.bounds.size
+        let zoomX = scrollViewSize.width / imageViewsize.width
+        let zoomY = scrollViewSize.height / imageViewsize.height
+        
+        let minZooming = min(zoomX,zoomY)
+        scrollView.minimumZoomScale = minZooming
+        scrollView.maximumZoomScale = 1.5
+        scrollView.zoomScale = minZooming
+    }
+    override func viewWillLayoutSubviews() {
+        setZoomParametersForSize(scrollView.bounds.size)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 }
 
+
 extension ViewController: UIScrollViewDelegate{
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
     func scrollViewDidScroll(scrollView: UIScrollView) {
         originLabel.text = "\(scrollView.contentOffset)"
         originLabel.sizeToFit()
