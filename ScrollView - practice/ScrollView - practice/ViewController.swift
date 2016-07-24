863cc9af235ee210cd4e2c43644e0e73e3c4a171
scau_ox <645670644@qq.com>
2016-07-24 22:12:23 +0800

    Learn How to scroll by coding

--- ScrollView - practice/ScrollView - practice/ViewController.swift
+++ ScrollView - practice/ScrollView - practice/ViewController.swift
@@ -9,9 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var originLabel = UILabel(frame: CGRect(x: 20, y: 30, width: 0, height: 0))
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let imageView = UIImageView(image: UIImage(named: "zombies.jpg"))
+        let srollView = UIScrollView(frame: view.bounds)
+        srollView.autoresizingMask = [.FlexibleWidth , .FlexibleHeight]
+        srollView.backgroundColor = UIColor.blackColor()
+        srollView.contentSize = imageView.bounds.size
+        
+        srollView.addSubview(imageView)
+        view.addSubview(srollView)
+        
+        //set content offSet
+        srollView.contentOffset = CGPoint(x: 60, y: 342.5)
+        
+        
+        originLabel.backgroundColor = UIColor.blackColor()
+        originLabel.textColor = UIColor.whiteColor()
+        
+        view.addSubview(originLabel)
+        srollView.delegate = self
         
     }
 
@@ -19,7 +40,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension ViewController: UIScrollViewDelegate{
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        originLabel.text = "\(scrollView.contentOffset)"
+        originLabel.sizeToFit()
+    }
 }
 
