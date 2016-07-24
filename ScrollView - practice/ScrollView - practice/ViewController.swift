751d6a06f67022cf4d3afde920b3e48a1bffb8ec
scau_ox <645670644@qq.com>
2016-07-25 01:35:54 +0800

    centre the imageView in scrollView

--- ScrollView - practice/ScrollView - practice/ViewController.swift
+++ ScrollView - practice/ScrollView - practice/ViewController.swift
@@ -40,6 +40,20 @@ class ViewController: UIViewController {
         setZoomParametersForSize(scrollView.bounds.size)
         
     }
+    
+    func reCenterImage() {
+        let scrollViewSize = scrollView.bounds.size
+        print("scrollView.bounds.size\(scrollViewSize)")
+        print("scrollView.frame.size\(scrollView.frame.size)")
+        let imageViewSize = imageView.frame.size
+        print("imageView.frame.size\(imageView.frame.size)")
+        print("imageView.bounds.size\(imageView.bounds.size)")
+        let horizontalSpace  = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
+        let VorizontalSpace = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
+        scrollView.contentInset = UIEdgeInsets(top: VorizontalSpace, left: horizontalSpace, bottom: VorizontalSpace, right: horizontalSpace)
+        
+    }
+    
     func setZoomParametersForSize(scrollViewSize: CGSize){
         let imageViewsize = imageView.bounds.size
         let zoomX = scrollViewSize.width / imageViewsize.width
@@ -52,11 +66,21 @@ class ViewController: UIViewController {
     }
     override func viewWillLayoutSubviews() {
         setZoomParametersForSize(scrollView.bounds.size)
+        reCenterImage()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        let centerPoint = CGPoint(x: scrollView.contentOffset.x + CGRectGetWidth(scrollView.bounds) / 2 , y: scrollView.contentOffset.y + CGRectGetHeight(scrollView.bounds) / 2 )
+        coordinator.animateAlongsideTransition({
+            (context) -> Void in
+            self.scrollView.contentOffset = CGPoint(x: centerPoint.x - size.width / 2, y: centerPoint.y - size.height / 2)
+            }, completion: nil)
+        
     }
 }
 
